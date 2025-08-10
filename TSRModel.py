b71adaf7a2ef9dbde957012b744fc97b8f6701ef
Shehan Ranawaka <lahirushehan164@gmail.com>
2025-08-10 12:32:50 +0200

    Update TSRModel  with trainer class

--- TSRModel.py
+++ TSRModel.py
@@ -1,6 +1,7 @@
 from keras.models import Sequential
 from keras.layers import Dense, Flatten, Dropout, Conv2D, MaxPooling2D
 from keras.optimizers import Adam
+import matplotlib.pyplot as plt
 
 class TSRModel:
     def __init__(self, input_shape, num_classes):
@@ -31,3 +32,37 @@ class TSRModel:
 
         model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.001), metrics=['accuracy'])
         return model
+    
+
+class Trainer:
+    def __init__(self, model, X_train, y_train, X_val, y_val, generator, steps_per_epoch_val, batch_size=32, epochs=10):
+        self.model = model
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_val = X_val
+        self.y_val = y_val
+        self.generator = generator
+        self.steps_per_epoch_val = steps_per_epoch_val
+        self.batch_size = batch_size
+        self.epochs = epochs
+
+    def train(self):
+        history=self.model.fit_generator(self.generator.flow(self.X_train,self.y_train,batch_size=self.batch_size),steps_per_epoch=self.steps_per_epoch_val,
+                                         epochs=self.epochs,validation_data=(self.X_val,self.y_val),shuffle=1)
+        return history
+    
+    def plot_history(self, history):
+        plt.figure(1)
+        plt.plot(history.history['loss'], label='Train Loss')
+        plt.plot(history.history['val_loss'], label='Validation Loss')
+        plt.legend(['training', 'validation'])
+        plt.title('Loss over epochs')
+        plt.xlabel('epoch')
+
+        plt.figure(2)
+        plt.plot(history.history['accuracy'], label='Train Accuracy')
+        plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+        plt.legend(['training', 'validation'])
+        plt.title('Accuracy')
+        plt.xlabel('epoch')
+        plt.show()
