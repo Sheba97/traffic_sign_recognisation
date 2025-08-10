b53a367b20cf410d7cedd41a2583a0460a5c6bdc
Shehan Ranawaka <lahirushehan164@gmail.com>
2025-08-10 12:34:07 +0200

    Update main code with trainer and model class changes

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from DataLoader import DataLoader
 from DataVisualizer import *
 from ImagePreprocessor import *
 from LabelPreprocesor import *
-from TSRModel import TSRModel
+from TSRModel import *              # Importing the CNN TSRModel
+from TSRModelTorch import *         # Importing the PyTorch TSRModel
 
 def main():
     data_path = 'Traffic_Data'
@@ -26,8 +27,8 @@ def main():
     print(f"Number of classes: {num_classes}")
 
     data_visualizer = DataVisualizer(X_train, y_train, label_file, num_classes)
-    #data_visualizer.show_sample_images()
-    #data_visualizer.plot_distribution()
+    data_visualizer.show_sample_images()
+    data_visualizer.plot_distribution()
 
     # Preprocess images
     img_preprocessor = ImagePreprocessor()
@@ -47,11 +48,29 @@ def main():
     label_preprocessor = LabelPreprocessor(num_classes)
     y_train_encoded, y_val_encoded, y_test_encoded = label_preprocessor.process_all(y_train, y_val, y_test)
     
+    keras_model(image_dim, num_classes, X_train, X_test , y_train_encoded, y_test_encoded, X_validation, y_val_encoded, augmenter.generator,steps_per_epoch_val, batch_size_val, epochs_val)
+
+    #pytorch_model(image_dim, num_classes, X_train, y_train_encoded, X_test, y_test_encoded, epochs=10, lr=0.001)  
+
+def keras_model(image_dim, num_classes, X_train, X_test, y_train_encoded, y_test_encoded, X_validation, y_val_encoded, generator, steps_per_epoch_val, batch_size_val, epochs_val):
     # Build and compile the model
     model = TSRModel(image_dim, num_classes)
     tsr_model = model.model
-    print(tsr_model.summary())
-    history=tsr_model.fit_generator(augmenter.generator.flow(X_train,y_train_encoded,batch_size=batch_size_val),steps_per_epoch=steps_per_epoch_val,epochs=epochs_val,validation_data=(X_validation,y_val_encoded),shuffle=1)
+    
+    print(tsr_model.summary())  
+
+    # Train the model
+    trainer = Trainer(tsr_model, X_train, y_train_encoded, X_validation, y_val_encoded, generator, steps_per_epoch_val, batch_size_val, epochs_val)
+    history = trainer.train()
+    trainer.plot_history(history)
+
+    # Evaluate the model
+    test_loss, test_accuracy = tsr_model.evaluate(X_test, y_test_encoded, verbose="0")
+    print(f"Test Loss: {test_loss}, Test Accuracy: {test_accuracy}")
+
+    # Save the model
+    tsr_model.save("traffic_sign_cnn.h5")
+    print("Model saved as traffic_sign_cnn.h5")
 
 if __name__ == "__main__":
-    main()
+    main() 
